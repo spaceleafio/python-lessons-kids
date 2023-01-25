37ded962f296ac387ce123058c5316c3f8a50acc
spaceleafio <79560857+spaceleafio@users.noreply.github.com>
2023-01-24 18:19:10 -0700

    initial commit

---
+++ 01/main.py
@@ -0,0 +1,27 @@
+# Number Variables
+x = 1
+print(x)
+
+# String Variables
+bird1 = "Snowy"
+bird2 = "Azure"
+
+print(bird1)
+print(bird2)
+
+# Boolean Variables (True / False)
+is_bird = True
+is_fish = False
+
+print(is_bird)
+print(is_fish)
+
+# Lists
+numbers = [1, 2, 3, 4]
+birds = ["Snowy", "Azure", "Hawk", "Eagle"]
+mixed = [1, "Cars", 3, "House"]
+
+print(birds[0])
+print(birds[1])
+print(birds[2])
+print(birds[3])
